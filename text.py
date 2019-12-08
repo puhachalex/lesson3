2b620acc454e5291dffe5ec804bb436708ac1782
Puhach Alex <puhach.alex@gmail.com>
2019-12-08 22:45:18 +0200

    xxx

---
+++ text.py
@@ -0,0 +1,42 @@
+a = 0
+c = 0
+
+with open ("/home/alex/my/projects/lesson3/referat.txt","r", encoding="utf-8") as f:
+
+    for symb in f:
+        d = len(str(f))
+        c = c + d
+print(f"Длина строки {c}")  
+
+with open ("/home/alex/my/projects/lesson3/referat.txt","r", encoding="utf-8") as f:
+    
+    for word in f:
+        word = word.split()
+        b=len(word)
+        a = a + b
+print(f"Количество слов {a}")
+
+with open ("/home/alex/my/projects/lesson3/referat.txt","r", encoding="utf-8") as f:
+
+    for x in f:
+        y = x.replace(".","!")
+        y = y.replace("\n","")
+        print(y)
+        
+with  open ("/home/alex/my/projects/lesson3/referat2.txt", "w", encoding="utf-8")as new:
+        new.write(y) 
+
+
+
+   
+
+        
+
+
+
+
+
+
+    
+    
+
