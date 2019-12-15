67a0d6fc455882a0c23a7b131a53dcc710a6eb7e
puhachalex <puhach.alex@gmail.com>
2019-12-15 15:42:10 +0200

    победа

--- text_.py
+++ text_.py
@@ -1,27 +1,33 @@
-a = 0
-c = 0
-z = str()
+def my_file():
 
-with open ("/home/alex/my/projects/lesson3/referat.txt","r", encoding="utf-8") as f:
+    a = 0
+    c = 0
+    z = str()
 
-    for i in f:
-        
-        b=len(i.split())
-        a = a + b
-        
-        d = len(str(f))
-        c = c + d
+    with open ("C:/projects/lesson3/referat.txt","r", encoding="utf-8") as f:
 
-        y = i.replace(".","!")
-        y = y.replace("\n","")
-        z = z + "\n" + y
+        for i in f:
+            
+            b=len(i.split())
+            a = a + b
+            
+            d = len(str(f))
+            c = c + d
+
+            y = i.replace(".","!")
+            y = y.replace("\n","")
+            z = z + "\n" + y
+
+               
+    with  open ("C:/projects/lesson3/referat2.txt", "w", encoding="utf-8") as new:
+        new.write(z)
+        new.write("\n\n"f"Количество слов {a}")
+        new.write("\n"f"Длина строки {c}")
+
+my_file()
 
 
 
-with  open ("/home/alex/my/projects/lesson3/referat2.txt", "w", encoding="utf-8") as new:
-    new.write(z)
-    new.write("\n\n"f"Количество слов {a}")
-    new.write("\n"f"Длина строки {c}")
     
 
 
